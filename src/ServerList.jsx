3ae541f75e73b820626536e12ce8344998ca8096
CosminPerRam <cosmin.p@live.com>
2023-06-02 22:09:24 +0300

    Fix table resizing (somewhat)

--- src/ServerList.jsx
+++ src/ServerList.jsx
@@ -5,21 +5,25 @@ import iconReplayColumn from './iconReplayColumn.png'
 export const ServerList = () => {
     return (
         <div class='ServerList' >
-            <table class='Servers' cellSpacing={0} >
+            <table class='ServersTable' >
                 <thead class='Headings'>
                     <tr>
                         <td class='IconHeading'>
                             <img src={iconPasswordColumn} />
                         </td>
-                        <td class='IconHeading'><img src={iconRobotronColumn} /></td>
-                        <td class='IconHeading'><img src={iconReplayColumn} /></td>
-                        <td class='Heading'style={{ flexGrow: 1 }}><div class='da'>Servers</div></td>
-                        <td class='Heading'>Game</td>
+                        <td class='IconHeading'>
+                            <img src={iconRobotronColumn} />
+                        </td>
+                        <td class='IconHeading'>
+                            <img src={iconReplayColumn} />
+                        </td>
+                        <td class='ResizableHeading'>Servers</td>
+                        <td class='ResizableHeading'>Game</td>
                         <td class='Heading'>Players</td>
                         <td class='Heading'>Bots</td>
-                        <td class='Heading'>Map</td>
+                        <td class='ResizableHeading'>Map</td>
                         <td class='Heading'>Latency</td>
-                        <td class='Heading' >Tags</td>
+                        <td class='Heading'>Tags</td>
                     </tr>
                 </thead>
                 <tbody>

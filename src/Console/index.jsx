8283529d21430ae1c7fad370ee0abc2e68bbef0f
CosminPerRam <cosmin.p@live.com>
2023-06-03 19:09:45 +0300

    Make console resizable and add title bar

--- src/Console/index.jsx
+++ src/Console/index.jsx
@@ -1,14 +1,20 @@
 import './style.scss'
 import Draggable from "react-draggable";
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Console = () => {
   return (
     <Draggable
       handle=".ConsoleHeader"
+      defaultPosition={{x: 240, y: 160}}
       >
       <div className={'ConsoleContainer'}>
         <div className={'ConsoleHeader'}>
-
+          <span className={'ConsoleHeaderTitle'}>
+            Console
+          </span>
+          <FontAwesomeIcon icon={faXmark} className={'ConsoleHeaderX'} />
         </div>
       </div>
     </Draggable>
--- src/TitleBar.jsx
+++ src/TitleBar.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faXmark } from "@fortawesome/free-solid-svg-icons"
 import { Title } from "./Title"
+
 export const TitleBar = () => {
     return (
         <div class='TitleBar'>

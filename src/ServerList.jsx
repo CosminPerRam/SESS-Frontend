c3c57120d0be677bb91146a693347dfce41c1f43
teooko <teobogdann@yahoo.com>
2023-06-03 11:47:15 +0300

    Somewhat fix it

--- src/ServerList.jsx
+++ src/ServerList.jsx
@@ -5,32 +5,61 @@ import iconReplayColumn from './iconReplayColumn.png'
 export const ServerList = () => {
     return (
         <div class='ServerList' >
-            <table class='ServersTable' >
-                <thead class='Headings'>
-                    <tr>
-                        <td class='IconHeading'>
-                            <img src={iconPasswordColumn} />
-                        </td>
-                        <td class='IconHeading'>
-                            <img src={iconRobotronColumn} />
-                        </td>
-                        <td class='IconHeading'>
-                            <img src={iconReplayColumn} />
-                        </td>
-                        <td class='ResizableHeading'>Servers</td>
-                        <td class='ResizableHeading'>Game</td>
-                        <td class='Heading'>Players</td>
-                        <td class='Heading'>Bots</td>
-                        <td class='ResizableHeading'>Map</td>
-                        <td class='Heading'>Latency</td>
-                        <td class='Heading'>Tags</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    </tr>
-                </tbody>
-            </table>
+            <div class='head'></div>
+            <div class='Table'>
+
+                <div class='Layer first' style={{minWidth: '400px'}}>
+                    <div class='texts'>
+                        <div class='heading'>Servers</div>
+                        <div class='text'>Servers</div>
+                        <div class='text'>Servers</div>
+                    </div>
+                    <div class='Layer' style={{minWidth: '350px'}}>
+                        <div class='texts'>
+                            <div class='heading'>Game</div>
+                            <div class='text'>Servers</div>
+                            <div class='text'>Servers</div>
+                        </div>
+                        <div class='Layer' style={{minWidth: '300px'}}>
+                            <div class='texts'>
+                                <div class='heading'>Players</div>
+                                <div class='text'>Servers</div>
+                                <div class='text'>Servers</div>
+                            </div>
+                            <div class='Layer' style={{minWidth: '250px'}}>
+                                <div class='texts'>
+                                    <div class='heading'>Bots</div>
+                                    <div class='text'>Servers</div>
+                                    <div class='text'>Servers</div>
+                                </div>
+                                <div class='Layer' style={{minWidth: '200px'}}>
+                                    <div class='texts'>
+                                        <div class='heading'>Map</div>
+                                        <div class='text'>Servers</div>
+                                        <div class='text'>Servers</div>
+                                    </div>
+                                    <div class='Layer' style={{minWidth: '150px'}}>
+                                        <div class='texts'>
+                                            <div class='heading'>Latency</div>
+                                            <div class='text'>Servers</div>
+                                            <div class='text'>Servers</div>
+                                        </div>
+                                        <div class='Layer' style={{minWidth: '50px'}}>
+                                            <div class='texts'>
+                                                <div class='heading'>Tags</div>
+                                                <div class='text'>Servers</div>
+                                                <div class='text'>Servers</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }

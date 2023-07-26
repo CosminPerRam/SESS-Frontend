4498589be2c8b04188bed1cf5131cd29f625a54a
teooko <teobogdann@yahoo.com>
2023-07-26 21:18:14 +0300

    Remove onContexMenu on whole app to enable right clicking

--- src/app.js
+++ src/app.js
@@ -4,10 +4,7 @@ import {Layout} from "./layout";
 
 export const App = () => {
   return (
-    <div className={'app'}
-         onContextMenu={(e) => {
-             e.preventDefault();
-         }}>
+    <div className={'app'}>
       <ApolloConfiguration>
         <Console>
           <Layout />

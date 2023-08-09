fa1bd9b7bdaa19ef5212755fcfd5c16c373dfb63
CosminPerRam <cosmin.p@live.com>
2023-08-09 20:10:47 +0300

    Add Vercel Analytics

--- src/app.jsx
+++ src/app.jsx
@@ -2,10 +2,12 @@ import {ApolloConfiguration} from "./core/ApolloConfiguration";
 import {Console} from "./console";
 import {Layout} from "./layout";
 import {RenderActiveMenu} from "./helpers/RenderActiveMenu";
+import { Analytics } from '@vercel/analytics/react';
 
 export const App = () => {
   return (
     <div className={'app'}>
+      <Analytics />
       <ApolloConfiguration>
         <Console>
           <Layout />

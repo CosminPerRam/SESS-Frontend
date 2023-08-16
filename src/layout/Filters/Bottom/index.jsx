98b028fcab8b808f9e9c7038c984fb4cbed1da18
teooko <teobogdann@yahoo.com>
2023-08-16 22:22:22 +0300

    Add functionality to simplified list checkbox

--- src/layout/Filters/Bottom/index.jsx
+++ src/layout/Filters/Bottom/index.jsx
@@ -3,24 +3,36 @@ import { Button } from "../../../common/Button";
 import { Label } from "../../../common/Label";
 import { ActionButtons } from "./ActionButtons";
 import { useBottomLabel } from "../../../hooks/useBottomLabel";
+import { useState } from "react";
 
 export const Bottom = () => {
   const label = useBottomLabel();
+  const [checked, setChecked] = useState(false);
 
   return (
     <div className='bottom'>
-      <Checkbox title={"Simplified List"} />
-      <Button
-        title={"Filters"}
-        id={"filters"}
-        style={{
-          width: "106px",
-          marginLeft: "32px",
-          color: "$buttonText",
-          minWidth: "106px",
-        }}
+      <Checkbox
+        title={"Simplified List"}
+        checked={checked}
+        onChanged={() => setChecked(!checked)}
       />
-      <Label name={label} />
+      {checked ? (
+        <div className='label'></div>
+      ) : (
+        <>
+          <Button
+            title={"Filters"}
+            id={"filters"}
+            style={{
+              width: "106px",
+              marginLeft: "32px",
+              color: "$buttonText",
+              minWidth: "106px",
+            }}
+          />
+          <Label name={label} />
+        </>
+      )}
       <ActionButtons />
     </div>
   );

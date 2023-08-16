48df08f4aec7082603e8cc8033f9d02c5e9a7975
teooko <teobogdann@yahoo.com>
2023-08-16 16:38:35 +0300

    Create hook for bottom label

---
+++ src/hooks/useBottomLabel.jsx
@@ -0,0 +1,32 @@
+﻿import { useSelector } from "react-redux";
+export const useBottomLabel = () => {
+  const {
+    canBeFull,
+    canBeEmpty,
+    canHavePassword,
+    spectatorProxy,
+    isSecured,
+    runsMap,
+    location,
+  } = useSelector((state) => state.filters);
+
+  const latency = useSelector((state) => state.localFilters.latency);
+  const maxPlayerCount = Number(
+    useSelector((state) => state.localFilters.maxPlayerCount)
+  );
+
+  return `Team Fortress 2;
+    ${isSecured === null ? "" : isSecured ? " secure;" : " not secure;"}
+    ${location ? location + ";" : ""}
+    ${latency ? " latency " + latency + ";" : ""}
+    ${
+      isNaN(maxPlayerCount) || maxPlayerCount === 0
+        ? ""
+        : " max players <= " + maxPlayerCount + ";"
+    }
+    ${canBeFull ? "" : " is not full;"}
+    ${canBeEmpty ? "" : " is not empty;"}
+    ${canHavePassword ? "" : " has no password;"}
+    ${spectatorProxy ? " supports replays;" : ""}
+    ${runsMap}`;
+};
--- src/layout/Filters/Bottom/index.jsx
+++ src/layout/Filters/Bottom/index.jsx
@@ -2,40 +2,13 @@ import { Checkbox } from "../../../common/Checkbox";
 import { Button } from "../../../common/Button";
 import { Label } from "../../../common/Label";
 import { ActionButtons } from "./ActionButtons";
-import { useSelector } from "react-redux";
+import { useBottomLabel } from "../../../hooks/useBottomLabel";
 
 export const Bottom = () => {
-  // when selecting stuff from same state, make one single hook
-  const canBeFull = useSelector((state) => state.filters.canBeFull);
-  const canBeEmpty = useSelector((state) => state.filters.canBeEmpty);
-  const canHavePassword = useSelector((state) => state.filters.canHavePassword);
-  const spectatorProxy = useSelector((state) => state.filters.spectatorProxy);
-  const runsMap = useSelector((state) => state.filters.runsMap);
-  const maxPlayerCount = Number(
-    useSelector((state) => state.localFilters.maxPlayerCount),
-  );
-  const isSecured = useSelector((state) => state.filters.isSecured);
-  const location = useSelector((state) => state.filters.location);
-  const latency = useSelector((state) => state.localFilters.latency);
-
-  const label = `Team Fortress 2;
-    ${isSecured === null ? "" : isSecured ? " secure;" : " not secure;"}
-    ${location ? location + ";" : ""}
-    ${latency ? " latency " + latency + ";" : ""}
-    ${
-      isNaN(maxPlayerCount) || maxPlayerCount === 0
-        ? ""
-        : " max players <= " + maxPlayerCount + ";"
-    }
-    ${canBeFull ? "" : " is not full;"}
-    ${canBeEmpty ? "" : " is not empty;"}
-    ${canHavePassword ? "" : " has no password;"}
-    ${spectatorProxy ? " supports replays;" : ""}
-    ${runsMap}`;
-  // this could be extracted into a custom hook
+  const label = useBottomLabel();
 
   return (
-    <div className="bottom">
+    <div className='bottom'>
       <Checkbox title={"Simplified List"} />
       <Button
         title={"Filters"}

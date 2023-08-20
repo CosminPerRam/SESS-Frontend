618e1fbe4efcf2b68ec1b0a4a35fc6cfe45c6b2c
CosminPerRam <cosmin.p@live.com>
2023-08-20 14:04:23 +0300

    Fix Draggable strict mode ref warning

--- src/windows/Window/index.jsx
+++ src/windows/Window/index.jsx
@@ -7,7 +7,7 @@ export const Window = ({ children, title, nodeRef, className }) => {
   const dispatch = useDispatch();
 
   return (
-    <Draggable handle='.window-header' defaultPosition={{ x: 320, y: 110 }}>
+    <Draggable nodeRef={nodeRef} defaultPosition={{ x: 320, y: 110 }}>
       <div className={`window-container ${className}`} ref={nodeRef}>
         <Header
           onCloseClick={() => dispatch(setActiveWindow(null))}

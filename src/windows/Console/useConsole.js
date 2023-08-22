3fd06e8c696bf61013dac6144c9a266f138ac37d
CosminPerRam <cosmin.p@live.com>
2023-08-22 19:33:53 +0300

    Add initial console custom hook

---
+++ src/windows/Console/useConsole.js
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useCallback } from "react";
+import { addConsoleLine, setInputCommand } from "../../redux/window/slice";
+
+export const useConsole = () => {
+  const dispatch = useDispatch();
+
+  const currentCommand = useSelector((state) => state.window.inputCommand);
+
+  const updateCurrentCommand = useCallback((value) => {
+    dispatch(setInputCommand(value));
+  }, []);
+
+  const addCLine = useCallback((text, options = {}) => {
+    dispatch(addConsoleLine({ text, options }));
+  }, []);
+
+  const executeCommand = useCallback((value) => {
+    console.log(`Executing "${value}"...`);
+  }, []);
+
+  return {
+    currentCommand,
+    updateCurrentCommand,
+    addConsoleLine: addCLine,
+    executeCommand,
+  };
+};

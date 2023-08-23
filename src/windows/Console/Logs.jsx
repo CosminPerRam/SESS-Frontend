5660e11a3ad661b8493f52e0e09368f71f1ec665
teooko <teobogdann@yahoo.com>
2023-08-23 16:07:58 +0300

    Fix console not scrolling to the bottom

--- src/windows/Console/Logs.jsx
+++ src/windows/Console/Logs.jsx
@@ -4,18 +4,20 @@ export const Logs = () => {
   const lines = useSelector((state) => state.window.consoleLines);
   return (
     <div className={`console-logs`}>
-      {lines.map((line, index) => {
-        const { text, options } = line;
-        return (
-          <p
-            key={index}
-            className={`console-line`}
-            style={{ color: options.color || `#BEB6A3` }}
-          >
-            {text}
-          </p>
-        );
-      })}
+      {lines
+        .map((line, index) => {
+          const { text, options } = line;
+          return (
+            <p
+              key={index}
+              className={`console-line`}
+              style={{ color: options.color || `#BEB6A3` }}
+            >
+              {text}
+            </p>
+          );
+        })
+        .reverse()}
     </div>
   );
 };

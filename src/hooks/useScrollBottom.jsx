4263021fb3d57fb51697bea08390d5829a0d3595
teooko <teobogdann@yahoo.com>
2023-08-25 19:54:36 +0300

    Add hook for auto scroll on cosnsole

---
+++ src/hooks/useScrollBottom.jsx
@@ -0,0 +1,13 @@
+﻿import { useEffect, useRef } from "react";
+
+export const useScrollBottom = (content) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.scrollTop = ref.current.scrollHeight;
+    }
+  }, [content]);
+
+  return ref;
+};
--- src/windows/Console/Logs.jsx
+++ src/windows/Console/Logs.jsx
@@ -1,23 +1,24 @@
 import { useSelector } from "react-redux";
-
+import { useScrollBottom } from "../../hooks/useScrollBottom";
 export const Logs = () => {
   const lines = useSelector((state) => state.window.consoleLines);
+
+  const consoleLogsRef = useScrollBottom(lines);
+
   return (
-    <div className={`console-logs`}>
-      {lines
-        .map((line, index) => {
-          const { text, options } = line;
-          return (
-            <p
-              key={index}
-              className={`console-line`}
-              style={{ color: options.color || `#BEB6A3` }}
-            >
-              {text}
-            </p>
-          );
-        })
-        .reverse()}
+    <div className={`console-logs`} ref={consoleLogsRef}>
+      {lines.map((line, index) => {
+        const { text, options } = line;
+        return (
+          <p
+            key={index}
+            className={`console-line`}
+            style={{ color: options.color || `#BEB6A3` }}
+          >
+            {text}
+          </p>
+        );
+      })}
     </div>
   );
 };
